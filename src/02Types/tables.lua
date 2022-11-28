dee0de2d07da3d8eee4ac163d19bd47c8316de37
Raskolnikov <jerry.tann@endress.com>
2022-11-28 14:33:06 +0800

    ✨ finish tables lesson

---
+++ src/02Types/tables.lua
@@ -0,0 +1,31 @@
+-- the only data structure in lua!, they are js objects, associative arrays, etc
+
+-- initialization
+
+luamap = {}
+
+luamap['fuck'] = 'you'
+
+print(luamap['fuck'])
+
+print(luamap.fuck) --- hmm dot notation
+luamap.e = nil
+luamap.schweddy = 'balls'
+
+-- iterating
+
+a = {}
+for i=1,10 do
+  a[i] = io.read()
+end
+
+-- print the lines
+for i,line in ipairs(a) do
+  print("i"..i.."line"..line)
+end
+--[[
+why doesn't this work
+for i,line in ipairs(luamap) do
+  print("i"..i.."line"..line)
+end
+--]]
