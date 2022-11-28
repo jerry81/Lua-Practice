230d133225e104be20c1fef22ce6ca8ae3e08b09
Raskolnikov <jerry.tann@endress.com>
2022-11-28 13:58:17 +0800

    ✨ getting started

---
+++ src/01GettingStarted/globalvar.lua
@@ -0,0 +1,5 @@
+print(b)
+b = 10
+print(b)
+b = nil --- deletes the var
+print(b)
---
+++ src/01GettingStarted/lexical.lua
@@ -0,0 +1,9 @@
+_ij = "valid identifier"
+_AVOID_THESE = "avoid this (often reserved)"
+--[[
+i am a block comment
+--]]
+
+---[[
+print("uncomment trick, add an extra hyphen to the block comment start")
+--]]
